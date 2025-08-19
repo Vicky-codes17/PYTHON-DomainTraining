370ffa2292766ff005a8ea179075b54c7f696dc5
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-08-19 22:12:01 +0530

    List_Compreshion.py

---
+++ List_Compreshion.py
@@ -0,0 +1,7 @@
+### 3) Using List Comprehension,print all numbers from 1 to 100 are divisible by 5
+
+my_list = [i for i in range(1, 10)]
+print(my_list)
+
+multiply_list = [i for i in range(1,100) if i % 5 == 0]
+print(multiply_list)
