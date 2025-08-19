962c3125874f10b6fa1d14e5762e87d5fdfb51bf
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-08-19 22:11:20 +0530

    hollow_rectangle.py

---
+++ hollow_rectangle.py
@@ -0,0 +1,10 @@
+### Hollow Rectangle Pattern
+r = 5
+c = 10
+for i in range(r):
+    for j in range(c):
+        if i == 0 or i == r - 1 or j == 0 or j == c - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
