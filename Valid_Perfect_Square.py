16e5a112e31c23cc4cb0badb4030646f5384525b
Vignesh <153487240+Vicky-codes17@users.noreply.github.com>
2025-08-19 15:38:41 +0530

    Valid_Perfect_Square.py

--- Valid_Perfect_Square.py
+++ Valid_Perfect_Square.py
@@ -1,3 +1,5 @@
+### Given a positive integer num, return true if num is a perfect square or false otherwise.
+
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
         return ((num)**0.5)%1 == 0
