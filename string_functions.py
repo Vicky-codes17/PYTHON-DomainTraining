f41cdd22fb3e93de27184f4b74835a2442caf369
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-08-20 20:33:17 +0530

    string_functions.py

---
+++ string_functions.py
@@ -0,0 +1,16 @@
+a = "cse"
+
+### Case Convertions
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.title())
+print(a.swapcase())
+
+### Boolean Checks
+print(a.isalpha())  
+print(a.isdigit()) 
+print(a.isalnum()) 
+print(a.islower()) 
+print(a.isupper())  
+
