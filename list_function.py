d0ebc7bdc7a664b9c92db283a857f9a5d79fc473
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-08-21 17:51:44 +0530

    list_function.py

---
+++ list_function.py
@@ -0,0 +1,11 @@
+### Write a Python program to demonstrate the use of the sorted() function.
+
+a = [12,2,30,42,3,5,12]
+b = sorted(a)
+print(b)
+
+print("Reverse Sorted List")
+c = sorted(a,reverse=True)
+print(c)
+
+print(max(a))
