612120be03973d12a32d8704a3ae1ba4f7e1b518
Vicky-codes17 <rameshvignesh0706@gmail.com>
2025-08-23 12:05:12 +0530

    scheduling_algorithm.py

---
+++ scheduling_algorithm.py
@@ -0,0 +1,18 @@
+import random
+
+no = int(input("Enter the number of teams :"))
+teams =[]
+matches = []
+
+for i in range(no):
+    d = input("Enter the team name : ")
+    teams.append(d)
+meet = int(input("Enter the number of times each team should meet : "))
+for i in range(no-1):
+    for j in range(i+1,no):
+        for k in range(meet):
+            matches.append((teams[i],teams[j]))
+random.shuffle(matches)
+
+for i in range(len(matches)):
+    print("Match {}: {} vs {}".format(i+1,matches[i][0],matches[i][1]))
